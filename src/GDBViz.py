9dac2c82525d0022c2c48ae8dac52e9bc6dba631
Amogh Prajapati <amprajap@unix1.csc.calpoly.edu>
2024-05-28 16:21:31 -0700

    Enabled multi-command queries

--- src/GDBViz.py
+++ src/GDBViz.py
@@ -21,18 +21,21 @@ def generate_viz_commands(info_local_data):
     ''' generate commands here based on info_local_data '''
     command_list.append("print x")
     command_list.append("print x")
+    command_list.append("info locals")
+    command_list.append("info locals")
     command_list.append("print x")
     command_list.append("info locals")
-    command_list.append("info locals")
-    command_list.append("info locals")
-
-
+    command_list.append("print x")
+    command_list.append("print &x")
     return command_list
 
 def execute_viz(result_data, file_name=None):
     # generates the visualization of memory based on result_data
     # if no file_name specified, use generic name
-    print("This will be implemeneted")
+    print("THIS IS MOGHY PRINITNG ------------>")
+    for res in result_data:
+        print(res)
+    print("<------------- PRINTING OVER")
 
 if __name__ == "__main__": 
     
@@ -53,35 +56,35 @@ if __name__ == "__main__":
     gdb = Connection(gdb_arugments)
     gdb.initialize()
 
+    '''
+        Multi-Command Mode, needed to run queries that need multiple gdb commands to get all the data
+        - Only commands that end in (gdb) can be included in multi-command queries 
+        - a multi-command query is represented as a [] of commands (which serves as an input for gdb)
+        - a result of a multi-command query is a [] of strings which repersents the result to the corresponding command, i.e.
+          input[0] will have a respond in output[0]
+        - during a multi-command query, the user input will be disabled in order to prevent any disturbance and be reenabled afterwards
+    
+    '''
+    mc_mode = False # Multi-command
+    mc_input = None
+    mc_output = None
+    sc_output = "" # Single-command output (used to read the full response for each command inside a multi-command query)
+    sc_ready = False
+
     read_list = [sys.stdin, gdb.stdout_pipe, gdb.stderr_pipe]
-
-    # needed to run multi command queries 
-    multi_command_mode = False
-    command_list = None
-    result_list = None
-
-    # used to store the result of an individual command in multi-command statement
-    # this means that multi-command queries only work with commands that finish in (gdb)
-    command_buffer = "" 
-    command_ready = False
-
     while True: 
-        # disable user input while running multi-command query
-        if multi_command_mode and sys.stdin in read_list: 
+
+        # enabling / disabling user's stdin for mc_mode
+        if mc_mode and sys.stdin in read_list:
             read_list.remove(sys.stdin)
-        # re-enable user input when back multi-command query finishes 
-        elif not multi_command_mode and sys.stdin not in read_list:
+        elif not mc_mode and sys.stdin not in read_list: 
             read_list.append(sys.stdin)
 
         readable, _, _ = select.select(read_list, [], [])
 
-        '''
-            Read Order if multiple fds are avalaible to read: 
-                1. gdb stderr
-                2. gdb stdout 
-                3. GDBViz stdin
-        '''
+        # Read Order: gdb stderr -> gdb stdout -> user stdin (or mc query commands)
 
+        # gdb STDERR
         if gdb.stderr_pipe in readable:
             stderr_buffer = ""
             stderr_pipe_read = read_fd(gdb.stderr_pipe)
@@ -90,66 +93,59 @@ if __name__ == "__main__":
                 stderr_pipe_read = read_fd(gdb.stderr_pipe)
             print(stderr_buffer)
                 
+        # gdb STDOUT
         if gdb.stdout_pipe in readable:
             stdout_buffer = ""
             stdout_pipe_read = read_fd(gdb.stdout_pipe)
             while len(stdout_pipe_read) != 0: 
-                if multi_command_mode:
-                    command_buffer += stdout_pipe_read
-                    if "(gdb)" in command_buffer:
-                        command_ready = True
+                if mc_mode: 
+                    # keep reading gdb's stdout until you reach a "(gdb)" to confirm the result of each command 
+                    sc_output += stdout_pipe_read
+                    if GDB_PHRASE in sc_output:
+                        sc_ready = True
                 else: 
                     stdout_buffer += stdout_pipe_read
                 stdout_pipe_read = read_fd(gdb.stdout_pipe)
 
-            if multi_command_mode and command_ready:
-                if command_list is None:
-                    # genrate a list of commands for multi query
-                    command_list = generate_viz_commands(command_buffer)
-                    result_list = []
-
-                    # run the first command 
-                    command = command_list.pop(0)
-                    print("THIS IS COMMAND: " + command)
-                    gdb.send(command + '\n')
-
-                    command_ready = False
-                    command_ready = ""
-                else: 
-                    # reformat for easier reading 
-                    gdb_phrase_index = command_buffer.find(GDB_PHRASE)
-                    result = command_buffer[:gdb_phrase_index].replace("\n", " ; ")
+            if mc_mode and sc_ready: 
+                # format sc_output correctly 
+                result = sc_output[:sc_output.find(GDB_PHRASE)]
+                if mc_input is None and mc_output is None:
+                    mc_input = generate_viz_commands(result)
+                    mc_output = []
+                else:
+                    result = result.replace("\n", " ; ")
                     result = result.strip(" ; ")
-                    result_list.append(result)
-            
-                    if len(command_list) > 0: 
-                        command = command_list.pop(0)
-                        print("THIS IS COMMAND: " + command)
-                        gdb.send(command + '\n')
-                    else: 
-                        print("MOGHY TESTING -------------------->")
-                        print(result_list)
-                        print("<__________________________________")
-                        execute_viz(result_list)
-                        multi_command_mode = False
-                        command_list = None
-                        result_list = None
+                    mc_output.append(result)
 
-                    command_ready = False
-                    command_buffer = command_buffer[:gdb_phrase_index + len(GDB_PHRASE)]
-                        
-            else:
+                # set sc_output for next command in mc query 
+                sc_ready = False
+                if len(sc_output) > sc_output.find(GDB_PHRASE) + len(GDB_PHRASE):
+                    sc_output = sc_output[sc_output.find(GDB_PHRASE) + len(GDB_PHRASE):]
+                else: 
+                    sc_output = ""
+
+                # if avalaible run next command 
+                if len(mc_input) > 0: 
+                    gdb.send(mc_input.pop(0) + '\n')
+                else: 
+                    execute_viz(mc_output)
+                    mc_mode = False
+                    mc_input = None
+                    mc_output = None
+            elif not mc_mode:
+                # print output to stdout as regular
                 print(stdout_buffer)
 
+        # USER STDIN
         if sys.stdin in readable:
             stdin_read = sys.stdin.readline().strip()
             if stdin_read.lower() == "stop":
                 gdb.terminate()
                 sys.exit(0)    
             elif len(stdin_read) >= 3 and stdin_read[:3].lower() == "viz": 
-                # viz [optional file name]
-                multi_command_mode = True
-                gdb.send("print x" + '\n')
+                mc_mode = True
+                gdb.send(INFO_COMMAND + '\n')
             else: 
                 gdb.send(stdin_read + '\n')
 
